fd11b0ae5a77828f30dc27426c2b48b90bafc861
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2025-03-21 11:14:58 -0400

    created lobby manager

--- Assets/Scripts/Networking/LobbyManager.cs
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -6,20 +6,22 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class OurNetwork : MonoBehaviour
+public class LobbyManager : MonoBehaviour
 {
     private Lobby currentLobby;
     public bool isHost = false;
     private MenuManager menuManager;
-
+    private OurNetwork ourNetwork;
+    
     // Mapping from authentication to player index
     private Dictionary<string, int> playerIndexMap = new Dictionary<string, int>();
     // Mapping from authentication to netcode id
     private Dictionary<string, ulong> playerIdToClientIdMap = new Dictionary<string, ulong>();
 
-    public async void Initialize(MenuManager manager)
+    public async void Initialize(MenuManager manager, OurNetwork network)
     {
         menuManager = manager;
+        ourNetwork = network;
 
         await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
@@ -124,46 +126,4 @@ public class OurNetwork : MonoBehaviour
     {
         return AuthenticationService.Instance.PlayerId;
     }
-
-    public void SendVibrationSignal(int playerToVibrate)
-    {
-        // foreach (var player in playerIndexMap)
-        // {
-        //     string playerID = player.Key;
-        //     int playerIndex = player.Value;
-
-        //     if (playerIndex == playerToVibrate)
-        //     {
-        //         // Find the targeted client's network ID
-        //         ulong targetClientId = GetClientIdByPlayerId(playerID);
-        //         if (targetClientId != 0)
-        //         {
-        //             ClientRpcParams clientRpcParams = new ClientRpcParams
-        //             {
-        //                 Send = new ClientRpcSendParams
-        //                 {
-        //                     TargetClientIds = new ulong[] { targetClientId }
-        //                 }
-        //             };
-
-        //             RpcVibratePhoneClientRpc(clientRpcParams);
-        //         }
-        //     }
-        // }
-    }
-
-    private ulong GetClientIdByPlayerId(string playerId)
-    {
-        return playerIdToClientIdMap[playerId];
-    }
-
-
-    // [Unity.Netcode.ClientRpc]
-    // public void RpcVibratePhoneClientRpc(ClientRpcParams clientRpcParams)
-    // {
-    // #if UNITY_ANDROID || UNITY_IOS
-    //     Handheld.Vibrate();
-    //     Debug.Log("Vibrating phone...");
-    // #endif
-    // }
 }
