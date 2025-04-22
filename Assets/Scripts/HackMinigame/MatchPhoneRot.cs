be2e37d12e082b7af53976163723e0246e1b5dd0
llama-soup <43183037+llama-soup@users.noreply.github.com>
2025-04-22 12:29:55 -0400

    lebron!!! (worked on minigame)

--- Assets/Scripts/HackMinigame/MatchPhoneRot.cs
+++ Assets/Scripts/HackMinigame/MatchPhoneRot.cs
@@ -14,35 +14,35 @@ public class GyroRotation : NetworkBehaviour
 
     public GameObject ballDownCollision;
 
-    void Start()
-    {
-        if (SystemInfo.supportsGyroscope)
-        {
-            gyro = Input.gyro;
-            gyro.enabled = true;
-            gyroAvailable = true;
+    // void Start()
+    // {
+    //     if (SystemInfo.supportsGyroscope)
+    //     {
+    //         gyro = Input.gyro;
+    //         gyro.enabled = true;
+    //         gyroAvailable = true;
 
             
-            // Capture the initial rotation as an inverse, so we can apply it as an offset
-            initialRotation = Quaternion.Inverse(ConvertGyroRotation(gyro.attitude));
-        }
-        else
-        {
-            Debug.LogError("Gyroscope not supported on this device.");
-        }
-    }
+    //         // Capture the initial rotation as an inverse, so we can apply it as an offset
+    //         initialRotation = Quaternion.Inverse(ConvertGyroRotation(gyro.attitude));
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Gyroscope not supported on this device.");
+    //     }
+    // }
 
-    void Update()
-    {
-        // if(IsServer == true)
-            // Apply the offset so the object starts with zeroed rotation
-            // avg = gyro_inputs.Average<Quaternion>();
-            transform.rotation = initialRotation * ConvertGyroRotation(gyro.attitude);
-    }
+    // void Update()
+    // {
+    //     // if(IsServer == true)
+    //         // Apply the offset so the object starts with zeroed rotation
+    //         // avg = gyro_inputs.Average<Quaternion>();
+    //         transform.rotation = initialRotation * ConvertGyroRotation(gyro.attitude);
+    // }
 
-    // Convert the gyroscope rotation to Unity's coordinate system
-    private Quaternion ConvertGyroRotation(Quaternion q)
-    {
-        return new Quaternion(q.x, q.z, q.y, -q.w);
-    }
+    // // Convert the gyroscope rotation to Unity's coordinate system
+    // private Quaternion ConvertGyroRotation(Quaternion q)
+    // {
+    //     return new Quaternion(q.x, q.z, q.y, -q.w);
+    // }
 }
