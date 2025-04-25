854a4f59b722610d9d4b37710fd3d26bcd5ab2d1
limilano <limilano@davidson.edu>
2025-04-25 11:35:18 -0400

    return to map button prefab

---
+++ Assets/Scripts/Map/ReturnToMap.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ReturnToMap : NetworkBehaviour
+{
+    [SerializeField] public GameObject backButton;
+    public override void OnNetworkSpawn()
+    {
+        CheckHost();
+        base.OnNetworkSpawn();
+    }
+
+    void CheckHost()
+    {
+        ulong clientId = NetworkManager.Singleton.LocalClientId;
+        if (clientId == GameManager.Instance.MapManager.hostId)
+            backButton.SetActive(true);
+        else
+            backButton.SetActive(false);
+
+    }
+
+    public void BackToMap()
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene("Map", LoadSceneMode.Single);
+    }
+}
