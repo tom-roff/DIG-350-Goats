0788d0941d599d4012453f21116561033166265f
Owen Mundy <omundy@gmail.com>
2025-04-27 10:33:46 -0400

    Adds dice model

---
+++ Assets/Plugins/Dice_6/Dice.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public enum DiceSide
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
